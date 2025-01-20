25174483ed8ef3b04e4167f95312abd3e33be214
Victor Stenmark <victor.stenmark22@gmail.com>
2025-01-20 10:24:37 +0100

    Make view_scored_images take mult metrics

--- test-pyiqa/read_data.py
+++ test-pyiqa/read_data.py
@@ -1,6 +1,5 @@
 import json
 from utils import log, bcolors
-from PIL import Image
 import os
 import cv2
 
@@ -31,20 +30,26 @@ def reverse_search(path, metric, score_range):
     min, max = score_range[0], score_range[1]
     return {k: v for k, v in metric_dict.items() if v >= min and v <= max}
 
-def view_scored_images(results_path, image_path, metric, score_range):
-    elems = reverse_search(results_path, metric, score_range)
-    for img_name, score in elems.items():
-        print(os.path.join(image_path, img_name))
-        image = cv2.imread(os.path.join(image_path, img_name))
-        cv2.imshow('Image', image)
-        log(f"{metric}: {img_name} -> {score}", bcolor_type=bcolors.OKBLUE)
-        print("Press any key to continue to the next image...")
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+def view_scored_images(results_path, image_path, metrics, score_range):
+    for metric in metrics:
+        log(f"Viewing images where {metric} score is in [{score_range[0], score_range[1]}]",
+            bcolor_type=bcolors.OKBLUE)
+        elems = reverse_search(results_path, metric, score_range)
+        for img_name, score in elems.items():
+            print(os.path.join(image_path, img_name))
+            image = cv2.imread(os.path.join(image_path, img_name))
+            cv2.imshow('Image', image)
+            log(f"{metric}: {img_name} -> {score}", bcolor_type=bcolors.OKBLUE)
+            print("Press any key to continue to the next image...")
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 def read_data(path):
     with open(path, 'r') as file:
         return json.load(file)
     
-#combine_metric_data("corrupted/results.json")s
-#view_scored_images("corrupted/metric_results.json", "corrupted", "hyperiqa", (0.3, 0.4))
+#combine_metric_data("corrupted/results.json")
+view_scored_images("corrupted/metric_results.json",
+                   "corrupted",
+                   ["hyperiqa", "ilniqe"],
+                   (0.3, 0.4))
