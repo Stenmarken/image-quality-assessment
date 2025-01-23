123a379239105a8c7f5c7a871159a49b44ea85de
Victor Stenmark <victor.stenmark22@gmail.com>
2025-01-23 14:03:17 +0100

    Add support for foggy images

--- test-pyiqa/corrupt_images.py
+++ test-pyiqa/corrupt_images.py
@@ -3,7 +3,6 @@ from PIL import Image
 import numpy as np
 import os
 from utils import bcolors, log
-import albumentations as A
 import albumentations.augmentations.functional as F
 import cv2
 import random
@@ -34,6 +33,17 @@ def add_rain(img_path, config):
                blur_value=config['blur_value'],
                brightness_coefficient=config['brightness_coefficient'],
                rain_drops=droplets)
+
+def add_fog(img_path, config):
+    random.seed(config['randomness_seed'])
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    droplets  = generate_droplets(image, config['fog_particle_share'], config['randomness_seed'])
+    return F.add_fog(img=image,
+                      fog_intensity=config['fog_intensity'],
+                      alpha_coef=config['alpha_coef'],
+                      fog_particle_positions=droplets,
+                      fog_particle_radiuses=[config['fog_particle_size']]*len(droplets))
 
 def get_corrupted_image(corruption, image_path, severity):
     if not os.path.isfile(image_path):
@@ -95,17 +105,40 @@ def generate_rain_configs(file_path):
 
     dicts = [dict(zip(keys, combination)) for combination in itertools.product(*values)]
     for d in dicts:
+        d['randomness_seed'] = config['randomness_seed']
         d['blur_value'] = int(d['blur_value'])
+    return dicts
+
+def generate_fog_configs(file_path):
+    config = {}
+    with open(file_path, 'r') as file:
+        config = yaml.safe_load(file)
+    fog_config = config['fog']
+    for k, v in fog_config.items():
+        # All lists except drop_color should be treated as ranges (start, stop, num_elements)
+        if isinstance(v, list):
+            start, stop, num = v
+            fog_config[k] = list(np.linspace(start, stop, num))
+        else:
+            fog_config[k] = [v]
+    keys = fog_config.keys()
+    values = fog_config.values()
+
+    dicts = [dict(zip(keys, combination)) for combination in itertools.product(*values)]
+    for d in dicts:
+        d['randomness_seed'] = config['randomness_seed']
     return dicts
 
 def generate_images(config_path, img_path, output_path):
     configs = generate_rain_configs(config_path)
+    #configs = generate_fog_configs(config_path)
     log(f"Generating {len(configs)} images", bcolor_type=bcolors.WARNING)
     file_to_config = {}
     for i, config in enumerate(configs):
         log("Generating image with config", bcolor_type=bcolors.OKBLUE)
         log(f"{config}", bcolor_type=bcolors.OKCYAN)
         img = add_rain(img_path=img_path, config=config)
+        #img = add_fog(img_path=img_path, config=config)
         img = Image.fromarray(img)
         path = os.path.join(output_path, f'{i}.png')
         os.makedirs(os.path.dirname(path), exist_ok=True)
