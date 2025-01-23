f6c7ee0898bbfda2155f26dfe022cd61e1570a18
Victor Stenmark <victor.stenmark22@gmail.com>
2025-01-23 14:33:59 +0100

    Remove imagecorruptions

--- test-pyiqa/corrupt_images.py
+++ test-pyiqa/corrupt_images.py
@@ -1,4 +1,3 @@
-from imagecorruptions import corrupt, get_corruption_names
 from PIL import Image
 import numpy as np
 import os
@@ -46,49 +45,6 @@ def add_fog(img_path, config):
                       fog_particle_positions=droplets,
                       fog_particle_radiuses=[config['fog_particle_size']]*len(droplets))
 
-def get_corrupted_image(corruption, image_path, severity):
-    if not os.path.isfile(image_path):
-        raise f"{image_path} is not a valid file. Crashing"
-    if corruption in ["glass_blur", "fog"]:
-        print(f"{bcolors.WARNING}Corruption type {corruption} is not supported. Returning{bcolors.ENDC}")
-        return
-    image_np = np.asarray(Image.open(image_path))
-    # corrupt returns a numpy ndarray, the same as the input
-    return corrupt(image_np, corruption_name=corruption, severity=severity)
-
-def get_safe_corruptions(corruptions = get_corruption_names()):
-    return [f for f in corruptions if f not in ["glass_blur", "fog"]]
-
-def save_image(output_path, full_path, img):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-    img = Image.fromarray(img)
-    img.save(full_path)
-
-def corrupt_image(img_path, img_name, 
-                  severity_range=range(1,6), 
-                  corruptions=get_corruption_names(), 
-                  output_path="corrupted/"):
-    images = {}
-    uncorrupted = np.asarray(Image.open(img_path))
-    # TODO: User should be able to specify where the images end up
-    save_image(output_path=output_path,
-               full_path=f"{output_path}/{img_name}_uncorrupted_0.png",
-               img=uncorrupted)
-    images['uncorrupted'] = {'0' : uncorrupted }
-    for corruption in get_safe_corruptions(corruptions):
-        sev_to_img = {}
-        for severity in severity_range:
-            log(msg=f"Corrupting {img_name} with type {corruption} and severity {severity}",
-                bcolor_type=bcolors.OKBLUE)
-            image = get_corrupted_image(corruption, img_path, severity)
-            sev_to_img[severity] = image
-            full_path = os.path.join(output_path, f"{img_name}_{corruption}_{severity}.png")
-
-            save_image(output_path=output_path, full_path=full_path, img=image)
-        images[corruption] = sev_to_img
-    return images
-
 def generate_rain_configs(config):
     rain_config = config['rain']
     for k, v in rain_config.items():
@@ -128,16 +84,12 @@ def load_config(path):
         return yaml.safe_load(file)
 
 def generate_images(img_path, output_path, configs, corrupt_func):
-    #configs = generate_rain_configs(config_path)
-    #configs = generate_fog_configs(config_path)
     log(f"Generating {len(configs)} images", bcolor_type=bcolors.WARNING)
     file_to_config = {}
     for i, config in enumerate(configs):
         log("Generating image with config", bcolor_type=bcolors.OKBLUE)
         log(f"{config}", bcolor_type=bcolors.OKCYAN)
-        #img = add_rain(img_path=img_path, config=config)
         img = corrupt_func(img_path=img_path, config=config)
-        #img = add_fog(img_path=img_path, config=config)
         img = Image.fromarray(img)
         path = os.path.join(output_path, f'{i}.png')
         os.makedirs(os.path.dirname(path), exist_ok=True)
